503ab4cfbbd9e71901986a34b1e954498004e4c5
ettemanaveen <ettemanaveen@gmail.com>
2026-04-19 16:34:28 -0600

    hi

--- character.h
+++ character.h
@@ -9,11 +9,11 @@ class Character {
     public:
         Character();
         Character(int number, string name, int agility, int focus, int luck);
-        int getNumber() const;
-        string getName() const;
-        int getAgility() const;
-        int getFocus() const;
-        int getLuck() const;
+        int getNumber();
+        string getName();
+        int getAgility();
+        int getFocus();
+        int getLuck();
         void setNumber(int number);
         void setName(string name);
         void setAgility(int agility);
