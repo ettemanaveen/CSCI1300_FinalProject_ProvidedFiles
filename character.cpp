71d5b86f06f398c41924ea1dd7207d8a35082f54
Porter Schmidt <posc5487@colorado.edu>
2026-04-19 16:56:05 -0600

    character.h & .cpp finished

--- character.cpp
+++ character.cpp
@@ -1,5 +1,59 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
 #include "character.h"
 
+Character::Character() {
+    number = 0;
+    name = "";
+    agility = 0;
+    focus = 0;
+    luck = 0;
+}
+
+Character::Character(int num, string n, int a, int f, int l) {
+    number = num;
+    name = n;
+    agility = a;
+    focus = f;
+    luck = l;
+}
+
+
+// ========== Getters ==========
 int Character::getNumber(){
     return number;
 }
+string Character::getName() {
+    return name;
+}
+int Character::getAgility() {
+    return agility;
+}
+int Character::getFocus() {
+    return focus;
+}
+int Character::getLuck() {
+    return luck;
+}
+
+// ========== Setters ==========
+void Character::setNumber(int n) {
+    number = n;
+}
+void Character::setName(string s) {
+    name = s;
+}
+void Character::setAgility(int a) {
+    agility = a;
+}
+void Character::setFocus(int f) {
+    focus = f;
+}
+void Character::setLuck(int l) {
+    luck = l;
+}
+
+
+
